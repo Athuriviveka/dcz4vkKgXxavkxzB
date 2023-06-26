492bcc3986dd02520caf5505966dc81ea20fde53
Athuri Vivekanantharasa <vivekaathuri@gmail.com>
2023-06-27 04:05:15 +0800

    EDA and model accuracy updated

--- splitData.py
+++ splitData.py
@@ -1,52 +1,53 @@
-# import StandardScaler as StandardScaler
 import pandas as pd
-import numpy as np
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import classification_report
-from sklearn.metrics import roc_auc_score
+from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+# from lightgbm import LGBMClassifier
 
 df = pd.read_csv('/users/BalakumaranSivarajan/Downloads/ACME-HappinessSurvey2020.csv')
-df['split'] = np.random.randn(df.shape[0], 1)
+print(df)
+df.info()
 
-msk = np.random.rand(len(df)) <= 0.7
+print(len(df)-len(df.drop_duplicates())) #finding duplicates to remove, because duplicates leads to overfitting
 
-train = df[msk]
-test = df[~msk]
-print(test.head())
+df = df.drop_duplicates()
+df.reset_index(drop=True, inplace=True)
+print(df)
+
+fig, ax = plt.subplots(figsize=(15,10))
+corr = df.corr()
+sns.heatmap(corr, annot=True)
+
+
+y = df['Y']
+X = df.drop(['Y'], axis=1)
+
+# Split into test and train
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
 
 features = ['X1', 'X2', 'X3', 'X4', 'X5', 'X6']
 target = ['Y']
 
-# Split into test and train
-X_train = train[features]
-y_train = train[target].to_numpy()
-X_test = test[features]
-y_test = test[target].to_numpy()
+knn = KNeighborsClassifier()
+rf = RandomForestClassifier()
+dt = DecisionTreeClassifier()
+# lgbm = LGBMClassifier()
 
-# # Normalize Features
-# scaler = StandardScaler()
-# X_train = scaler.fit_transform(X_train)
-# X_test = scaler.fit_transform(X_test)
+models = [knn, rf, dt]
 
 
-def run_model(model, X_train, y_train, X_test, y_test, verbose=True):
-    if verbose == False:
-        model.fit(X_train, y_train, verbose=0)
-    else:
+def test_models(l):
+    for model in models:
         model.fit(X_train, y_train)
-    y_pred = model.predict(X_test)
-    roc_auc = roc_auc_score(y_test, y_pred)
-    print("ROC_AUC = {}".format(roc_auc))
-    print(classification_report(y_test, y_pred, digits=5))
-    # plot_confusion_matrix(model, X_test, y_test, cmap=plt.cm.Blues, normalize='all')
+        y_predict = model.predict(X_test)
+        acc = accuracy_score(y_test, y_predict)
+        print(model)
+        print("\nThe Training Accuracy is : {} \n".format(model.score(X_train, y_train) * 100))
+        print('The Test Accuracy is : {}\n\n'.format(acc))
 
-    return model, roc_auc
 
-params_rf = {'max_depth': 25,
-         'min_samples_leaf': 1,
-         'min_samples_split': 2,
-         'n_estimators': 1200,
-         'random_state': 42}
-
-model_rf = RandomForestClassifier(**params_rf)
-model_rf, roc_auc_rf = run_model(model_rf, X_train, y_train, X_test, y_test)
+test_models(models)
